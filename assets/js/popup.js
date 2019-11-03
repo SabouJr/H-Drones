026b1deaa9a72867c72b03a74026ba1c6842aaf7
Clement <clement.sabourin@hetic.net>
2019-11-03 23:39:58 +0100

    Site V1

---
+++ assets/js/popup.js
@@ -0,0 +1,9 @@
+function openSection(idElem) {
+    console.log(idElem);
+    document.getElementById(idElem).style.top ="100px";
+}
+
+function closeSection(idElem) {
+    console.log(idElem);
+    document.getElementById(idElem).style.top ="-500px";
+}
