a261bead7242a15941723a0642c5d72809546160
Lenndeums <lennydemu@gmail.com>
2019-11-13 17:25:05 +0100

    video home + arrow

--- assets/js/home.js
+++ assets/js/home.js
@@ -74,3 +74,14 @@ $(document).ready(function() {
    $("#video-fond").removeClass("remove-video");
  });
 });
+
+// Scroll Effect
+$(document).ready(function() {
+	$('.js-scrollTo').on('click', function() { // Au clic sur un élément
+    console.log("test");
+		var page = $(this).attr('href'); // Page cible
+		var speed = 750; // Durée de l'animation (en ms)
+		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+		return false;
+	});
+});
