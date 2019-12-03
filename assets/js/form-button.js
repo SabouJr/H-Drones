cc1c09334f4a959a122e429257ef0635ac0a865c
Clement <clement.sabourin@hetic.net>
2019-12-03 16:45:07 +0100

    Form php

--- assets/js/form-button.js
+++ assets/js/form-button.js
@@ -1,14 +1,3 @@
-$('.order').click(function(e) {
-    e.preventDefault();
-    let button = $(this);
-    if(!button.hasClass('animate')) {
-        button.addClass('animate');
-        setTimeout(() => {
-            button.removeClass('animate');
-        }, 6500);
-    }
-});
-
 // Loader
  $(window).on('load', function(){
    $("#loader").fadeOut("slow");
