abc1d2fb8ac164abec9d51ba1013b01ca74c1afc
Lenndeums <lennydemu@gmail.com>
2019-12-10 13:39:15 +0100

    anim arrow down

--- assets/js/home.js
+++ assets/js/home.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 // Scroll Effect Arrow
 $(document).ready(function() {
 	$('.js-scrollTo').on('click', function() { // Au clic sur un élément
-		var page = $(this).attr('href'); // Page cible
+		var page = "#secteurs-part"; // Page cible
 		var speed = 750; // Durée de l'animation (en ms)
 		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
 		return false;
